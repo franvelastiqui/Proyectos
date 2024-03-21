6454586fa04e8b750ca898567a272ad551223076
franvelastiqui <47385918+franvelastiqui@users.noreply.github.com>
2024-03-21 17:54:05 -0300

    Add files via upload

---
+++ DocumentManagementSystem/DocumentManagementSystemAPI/DMS.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentManagementSystemAPI
+{
+    public class DMS
+    {
+        public DMS()
+        {
+
+        }
+
+        public bool CreateDocument(string name, string description, ref int id)
+        {
+            int webCallResult = 0;
+            bool success = false;
+
+            //Call to web method that adds new document to DMS goes here
+
+            webCallResult = 8; //call to web method returns 8 after adding new document to DMS
+
+            if (webCallResult != 0)
+            {
+                success = true;
+            }
+
+            id = webCallResult;
+
+            return success;
+        }
+        public int CreateDocument(string name)
+        {
+
+            //Call to web method that adds new document to DMS goes here
+            int webCallResult = 8;
+
+            return webCallResult;
+
+        }
+        public DMSDocumentBST GetDMSDocumentBST(int[] sortedArray)
+        {
+            return new DMSDocumentBST(sortedArray);
+        }
+    }
+}
---
+++ DocumentManagementSystem/DocumentManagementSystemAPI/Document.cs
@@ -0,0 +1,113 @@
+﻿using System.Security.Cryptography;
+using System.Xml.Linq;
+
+namespace DocumentManagementSystemAPI
+{
+    public enum DocumentMetaDataFormat
+    {
+        JSON,
+        XML,
+        CSV
+    }
+
+    public abstract class Document
+    {
+        private readonly int id;
+        private readonly string name;
+        private readonly string description;
+
+        private Document leftDoc;
+        private Document rightDoc;
+
+        protected Document(int id)
+        {
+            if (IsDocIdValid(id))
+            {
+                //Web service method call that retrieves document metadata goes here
+                this.id = id;
+                name = $"Doc{id}";
+                description = $"Document description for document: {id}";
+
+            }
+            else
+            {
+                throw new Exception("Invalid Id");
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+        }
+
+        public Document LeftDoc
+        {
+            get
+            {
+                return leftDoc;
+            }
+            set
+            {
+                leftDoc = value;
+            }
+        }
+        public Document RightDoc
+        {
+            get
+            {
+                return rightDoc;
+            }
+            set
+            {
+                rightDoc = value;
+            }
+        }
+
+        private bool IsValidId(int id)
+        {
+            if (!this.IsDocIdValid(id))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public virtual string GetSerializedDocumentMetadata(DocumentMetaDataFormat documentMetaDataFormat)
+        {
+            string result = null;
+
+            switch (documentMetaDataFormat)
+            {
+                case DocumentMetaDataFormat.JSON:
+                    throw new NotImplementedException();
+                case DocumentMetaDataFormat.XML:
+                    throw new NotImplementedException();
+                case DocumentMetaDataFormat.CSV:
+                    result = $"{Id},\"{Name}\",\"{Description}\"";
+                    break;
+                default:
+                    throw new Exception("Invalid document metadata format");
+            }
+            return result;
+        }
+
+        public abstract bool IsDocIdValid(int id);
+    }
+}
